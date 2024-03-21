156e5579b2c613a465f114092c119ae8c8ea1b8e
CassianeVitoria <127560232+CassianeVitoria@users.noreply.github.com>
2024-03-21 00:11:27 +0000

    fiz as atividade1, atividade2 e atividade3

---
+++ atividade2.php
@@ -0,0 +1,19 @@
+<?php
+/*
+if(isset($_POST["num1"]) && isset($_POST["num2"])){
+
+$n1=$_POST["num1"];
+$n2=$_POST["num2"];
+
+$soma=$n1+$n2;
+$subtração=$n1-$n2;
+$multiplicação=$n1*$n2;
+$divisão=$n1/$n2;
+
+echo" O resultado é $soma";
+echo" O resultado é $subtração";
+echo" O resultado é $multiplicação";
+echo" O resultado é $divisão";
+}
+
+?>
